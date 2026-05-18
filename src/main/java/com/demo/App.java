1b86117f016318c925950285742472e6553d4c8a
AkshayMS18 <106314152+AkshayMS18@users.noreply.github.com>
2026-05-18 13:50:12 +0530

    initial commit

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class App {
+    
+    public static void main( String[] args )
+    {
+        System.out.println("Hello this is a java project ");
+    }
+}
+
---
+++ src/test/java/com/demo/AppTest.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
